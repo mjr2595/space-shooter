8989a34011ecb1516a1bdf11c24739293f5d669a
Michael J Raymond <michaeljohnraymond@gmail.com>
2017-07-23 17:02:45 -0500

    explosions added

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+    public GameObject explosion;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary") {
+            return;
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
